f462752644f1efb13d419b7303acb85dfbc7eec2
js10081 <js10081008@gmail.com>
2022-02-22 16:37:38 +0900

    ScreenTransitionExample

---
+++ ScreenTransitionExample/SeguePushViewController.swift
@@ -0,0 +1,14 @@
+
+
+import UIKit
+
+class SeguePushViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tapBackButton(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
